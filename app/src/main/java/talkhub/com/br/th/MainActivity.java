522b9fbc40100348b5080a667ebc3e76f9f8879e
Gabriel Otahara <gabriel.otahara@gmail.com>
2017-10-21 11:54:08 -0200

    Implementação do menu option, na toolbar da main activity

--- app/src/main/java/talkhub/com/br/th/MainActivity.java
+++ app/src/main/java/talkhub/com/br/th/MainActivity.java
@@ -5,13 +5,20 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements Equipe.OnFragmentInteractionListener,Projeto.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements TabEquipe.OnFragmentInteractionListener,TabProjeto.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.menu_main);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
         tabLayout.addTab(tabLayout.newTab().setText("Equipes"));
@@ -44,5 +51,21 @@ public class MainActivity extends AppCompatActivity implements Equipe.OnFragment
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.menuSair){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
