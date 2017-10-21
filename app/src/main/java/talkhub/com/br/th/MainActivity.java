f04807e3f41e73a3b5737928b5cc0c18ae9f8bc4
Gabriel Otahara <gabriel.otahara@gmail.com>
2017-10-21 11:03:13 -0200

    Merge branch 'master' of C:\Users\gabri\AndroidStudioProjects\TH with conflicts.

--- app/src/main/java/talkhub/com/br/th/MainActivity.java
+++ app/src/main/java/talkhub/com/br/th/MainActivity.java
@@ -1,34 +1,17 @@
 package talkhub.com.br.th;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements TabEquipe.OnFragmentInteractionListener,TabProjeto.OnFragmentInteractionListener {
-
-
-
-
+public class MainActivity extends AppCompatActivity implements Equipe.OnFragmentInteractionListener,Projeto.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
-
-
-
-
-
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
         tabLayout.addTab(tabLayout.newTab().setText("Equipes"));
