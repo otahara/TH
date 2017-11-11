34614cf8e84ce1795b0d0bf6911954723605d608
Gabriel Otahara <gabriel.otahara@gmail.com>
2017-11-11 14:11:30 -0200

    Adicionado a activity do mural das equipes

---
+++ app/src/main/java/talkhub/com/br/th/MuralEquipeActivity.java
@@ -0,0 +1,13 @@
+package talkhub.com.br.th;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MuralEquipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mural_equipe);
+    }
+}
